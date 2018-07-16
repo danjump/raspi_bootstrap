da879e350e3690cbdfdc58d49777df7858e9a1a0
Daniel Jumper <daniel.jumper@gmail.com>
2018-07-15 21:00:55 -0500

    updated jupyter notebook config

--- install_software/install_python_packages.sh
+++ install_software/install_python_packages.sh
@@ -3,6 +3,8 @@
 conda install numpy scikit-learn pandas scipy matplotlib seaborn jupyter
 pip install tqdm
 pip install clashroyale
+pip install ipywidgets
 
 jupyter notebook --generate-config
+jupyter nbextension enable --py widgetsnbextension
 jupyter notebook password
