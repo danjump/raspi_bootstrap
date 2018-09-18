4f6fda0f89773fde8028511e007d81bcc79e809f
Daniel Jumper <daniel.jumper@gmail.com>
2018-09-17 21:21:15 -0500

    added google drive api python package install

--- install_software/install_python_packages.sh
+++ install_software/install_python_packages.sh
@@ -4,6 +4,8 @@ conda install numpy scikit-learn pandas scipy matplotlib seaborn jupyter
 pip install tqdm
 pip install clashroyale
 pip install ipywidgets
+pip install --upgrade google-api-python-client oauth2client
+
 
 jupyter notebook --generate-config
 jupyter nbextension enable --py widgetsnbextension
